3fecf8b3363fdbef184c666f53cb66528dc6193b
Nikita Konovalov <n.konovalov@galament.com>
2021-11-03 10:12:15 +0400

    add layout and fix problems

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   KeyboardDatePicker,
   MuiPickersUtilsProvider,
 } from "@material-ui/pickers";
-import { ListItem } from "@material-ui/core";
+import { ListItem, Typography } from "@material-ui/core";
 
 const KeyboardInputProps = {
   disableUnderline: true,
@@ -12,22 +12,30 @@ const KeyboardInputProps = {
 };
 const KeyboardButtonProps = { size: "small" };
 
+const Message = () => <div style={{ padding: '10px 20px 0'}}><Typography>Hello world!</Typography></div>;
+
 export const App = () => {
   const [selectedDate, handleDateChange] = useState(new Date());
 
   return (
     <MuiPickersUtilsProvider utils={MomentUtils}>
-      <ListItem divider>
-        <KeyboardDatePicker
-          fullWidth
-          clearable
-          autoOk
-          value={selectedDate}
-          onChange={handleDateChange}
-          InputProps={KeyboardInputProps}
-          KeyboardButtonProps={KeyboardButtonProps}
-        />
-      </ListItem>
+      <div style={{ width: 600, display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+        <ListItem divider>
+          <KeyboardDatePicker
+            fullWidth
+            clearable
+            autoOk
+            format="DD/MM/YYYY"
+            value={selectedDate}
+            onChange={handleDateChange}
+            InputProps={KeyboardInputProps}
+            KeyboardButtonProps={KeyboardButtonProps}
+            ToolbarComponent={Message}
+          />
+        </ListItem>
+        <ListItem divider>
+        </ListItem>
+      </div>
     </MuiPickersUtilsProvider>
   );
 };
