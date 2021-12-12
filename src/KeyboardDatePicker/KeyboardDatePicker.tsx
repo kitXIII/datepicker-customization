83bdccc5e555b98c6466edcc8a58d158808a40ad
Nikita Konovalov <mipo4t@gmail.com>
2021-12-12 18:31:39 +0400

    pretify arrows

--- src/KeyboardDatePicker/KeyboardDatePicker.tsx
+++ src/KeyboardDatePicker/KeyboardDatePicker.tsx
@@ -1,11 +1,10 @@
-import { useCallback, FC } from "react";
+import { useCallback, FC, useRef } from "react";
 import {
   DatePickerView,
   KeyboardDatePicker as MUIKeyboardDatePicker,
 } from "@material-ui/pickers";
 import { ToolbarComponentProps } from "@material-ui/pickers/Picker/Picker";
-import { PopoverOrigin } from "@material-ui/core";
-import { IconButtonProps } from "@material-ui/core/IconButton";
+import { makeStyles, PopoverOrigin } from "@material-ui/core";
 import { InputProps } from "@material-ui/core/Input/Input";
 import { DatePickerToolbar } from "./DatePickerToolbar";
 import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
@@ -16,11 +15,10 @@ const KeyboardInputProps: Partial<InputProps> = {
   fullWidth: true,
 };
 
-const KeyboardButtonProps: Partial<IconButtonProps> = { size: "small" };
 
 const PopoverOriginSettings: PopoverOrigin = {
   vertical: -5,
-  horizontal: 'center',
+  horizontal: "center",
 };
 
 const DatePickerViewOrder: DatePickerView[] = ["year", "month", "date"];
@@ -45,21 +43,58 @@ export const KeyboardDatePicker: FC<KeyboardDatePickerProps> = ({
     [message]
   );
 
+  const classes = useStyles();
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
-    <MUIKeyboardDatePicker
-      fullWidth
-      variant="inline"
-      format={format}
-      views={DatePickerViewOrder}
-      value={value}
-      onChange={onChange}
-      InputProps={KeyboardInputProps}
-      KeyboardButtonProps={KeyboardButtonProps}
-      ToolbarComponent={ToolbarComponent}
-      PopoverProps={{
-        transformOrigin: PopoverOriginSettings,
-        elevation: 4,
-      }}
-    />
+    <div className={classes.container} ref={containerRef}>
+      <MUIKeyboardDatePicker
+        className={classes.picker}
+        fullWidth
+        variant="inline"
+        format={format}
+        views={DatePickerViewOrder}
+        value={value}
+        onChange={onChange}
+        InputProps={KeyboardInputProps}
+        KeyboardButtonProps={{
+          className: classes.inputIcon || undefined,
+          size: "small",
+        }}
+        leftArrowButtonProps={{
+          size: "small",
+        }}
+        rightArrowButtonProps={{
+          size: "small",
+        }}
+        ToolbarComponent={ToolbarComponent}
+        PopoverProps={{
+          className: classes.popover,
+          anchorEl: containerRef.current,
+          transformOrigin: PopoverOriginSettings,
+          elevation: 4,
+        }}
+      />
+    </div>
   );
 };
+
+const useStyles = makeStyles({
+  container: {
+    width: "100%",
+  },
+  picker: {},
+  inputIcon: {
+    borderRadius: "4px",
+  },
+  popover: {
+    "& .MuiPickersCalendarHeader-switchHeader": {
+      padding: '0 12px',
+    },
+    "& .MuiPickersCalendarHeader-switchHeader .MuiIconButton-root": {
+      borderRadius: "4px",
+      width: '32px',
+      height: '32px',
+    },
+  },
+});
